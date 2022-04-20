d1643df8de25339423f80c39f3037965bddf5c3e
kasim-saifi <kasim@veeaargroup.com>
2022-04-20 16:08:39 +0530

    second commit by kasim saifi

--- vgo/vgoapi/views.py
+++ vgo/vgoapi/views.py
@@ -19,34 +19,35 @@ from django.shortcuts import render
 # Python program to illustrate the concept
 # of threading
 # importing the threading module
-import threading
 
-def print_cube(num):
-	"""
-	function to print cube of given num
-	"""
-	print("Cube: {}".format(num * num * num))
+# import threading
 
-def print_square(num):
-	"""
-	function to print square of given num
-	"""
-	print("Square: {}".format(num * num))
+# def print_cube(num):
+# 	"""
+# 	function to print cube of given num
+# 	"""
+# 	print("Cube: {}".format(num * num * num))
 
-# if __name__ == "__main__":
-	# creating thread
-	t1 = threading.Thread(target=print_square, args=(10,))
-	t2 = threading.Thread(target=print_cube, args=(10,))
+# def print_square(num):
+# 	"""
+# 	function to print square of given num
+# 	"""
+# 	print("Square: {}".format(num * num))
 
-	# starting thread 1
-	t1.start()
-	# starting thread 2
-	t2.start()
+# # if __name__ == "__main__":
+# 	# creating thread
+# 	t1 = threading.Thread(target=print_square, args=(10,))
+# 	t2 = threading.Thread(target=print_cube, args=(10,))
 
-	# wait until thread 1 is completely executed
-	t1.join()
-	# wait until thread 2 is completely executed
-	t2.join()
+# 	# starting thread 1
+# 	t1.start()
+# 	# starting thread 2
+# 	t2.start()
 
-	# both threads completely executed
-	print("Done!")
+# 	# wait until thread 1 is completely executed
+# 	t1.join()
+# 	# wait until thread 2 is completely executed
+# 	t2.join()
+
+# 	# both threads completely executed
+# 	print("Done!")
